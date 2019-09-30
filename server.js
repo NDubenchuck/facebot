ed6cf8901d112fc007832f668cf3d1cb874c6029
nickellodio <nickellodio@gmail.com>
2019-09-30 19:25:32 +0300

    fixed2

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app =express()
 
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'))
+  res.sendFile(path.join(__dirname, 'index.html'))
 })
 
 const PORT = process.env.PORT || 3000
